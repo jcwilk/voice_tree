3292fb25dfbd0a309e5fa130ef8061bf1e5a5b18
John Wilkinson <me@jcwilk.com>
2019-09-22 00:44:01 -0700

    Add in some autoresizing from a tutorial
    What, I'm rusty ^_^

--- public/js/game.js
+++ public/js/game.js
@@ -1,3 +1,46 @@
+class bootGame extends Phaser.Scene {
+    constructor() {
+        super("BootGame");
+    }
+    create() {
+        console.log("game is booting...");
+        this.scene.start("PlayGame");
+    }
+}
+
+class playGame extends Phaser.Scene {
+    constructor() {
+        super("PlayGame");
+    }
+    create() {
+        console.log("test");
+    }
+}
+
+var game;
 window.onload = function() {
-  var game = new Phaser.Game();
+  var gameConfig = {
+    width: 480,
+    height: 640,
+    scene: [bootGame, playGame]
+  }
+  game = new Phaser.Game(gameConfig);
+  window.focus()
+  resizeGame();
+  window.addEventListener("resize",resizeGame);
+}
+
+function resizeGame() {
+    var canvas = document.querySelector("canvas");
+    var windowWidth = window.innerWidth;
+    var windowHeight = window.innerHeight;
+    var windowRatio = windowWidth / windowHeight;
+    var gameRatio = game.config.width / game.config.height;
+    if (windowRatio < gameRatio) {
+        canvas.style.width = windowWidth + "px";
+        canvas.style.height = (windowWidth / gameRatio) + "px";
+    } else {
+        canvas.style.width = (windowHeight * gameRatio) + "px";
+        canvas.style.height = windowHeight + "px";
+    }
 }
