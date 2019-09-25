3ea1c6bed72a5704de10fe161939814e60e54999
John Wilkinson <me@jcwilk.com>
2019-09-24 19:42:56 -0700

    Mostly have the draw working, few bugs left
    Still need to make clicking on a leaf expand rather than just prepopulating things

--- public/js/game.js
+++ public/js/game.js
@@ -6,7 +6,7 @@ var gameOptions = {
 }
 
 var gameUtils = {}
-gameUtils.getTilePosition = function(row, col) {
+gameUtils.getTilePosition = function(col, row) {
   var posX = gameOptions.tileSize * col;
   var posY = gameOptions.tileSize * row;
   return new Phaser.Geom.Point(posX, posY);
@@ -48,9 +48,96 @@ class playGame extends Phaser.Scene {
   }
 
   create() {
-    var tilePosition = gameUtils.getTilePosition(0,0);
-    this.add.image(tilePosition.x, tilePosition.y, "tree_tiles", treeSprite.downNub).setOrigin(0,0);
-    this.add.image(tilePosition.x, tilePosition.y, "tree_tiles", treeSprite.leaf).setOrigin(0,0);
+    // var tilePosition = gameUtils.getTilePosition(0,0);
+    // this.add.image(tilePosition.x, tilePosition.y, "tree_tiles", treeSprite.downNub).setOrigin(0,0);
+    // this.add.image(tilePosition.x, tilePosition.y, "tree_tiles", treeSprite.leaf).setOrigin(0,0);
+    var node = makeNode();
+    node.replies.push(makeNode());
+    node.replies.push(makeNode());
+    growTreeNode(node, 0, -1, this);
+
+  }
+}
+
+function makeTreeSprite(col,row,frame,scene) {
+  console.log("sprite"+frame+":"+col+","+row);
+  var corner = gameUtils.getTilePosition(col,row);
+  return scene.add.image(corner.x, corner.y, "tree_tiles", frame).setOrigin(0,0);//.setVisible(false);
+
+}
+
+function makeNode() {
+  var obj = {
+    flower: null,
+    replies: [],
+    segments: [],
+    nub: null,
+    leaf: null,
+    sprite: null
+  }
+
+  //builds node, flower, and segments leading up to it
+  obj.buildNodeSprites = function(col, row, minWidth, frameIndex, scene) {
+    if (obj.sprite) obj.sprite.destroy();
+    if (obj.flower) obj.flower.destroy();
+
+    console.log("node"+col+","+row);
+
+    for(var i = 0; i < obj.segments.length; i++) obj.segments[i].destroy;
+    obj.segments = [];
+
+    var nodeWidth = 0;
+
+    for(var i = 0; i < minWidth-1; i++) {
+      obj.segments.push(makeTreeSprite(col + i, row, treeSprite.rightSegment, scene))
+      nodeWidth++;
+    }
+
+    obj.sprite = makeTreeSprite(col + nodeWidth, row, frameIndex, scene);
+    obj.flower = makeTreeSprite(col + nodeWidth, row, treeSprite.flower, scene);
+    nodeWidth++;
+    return nodeWidth;
+  }
+
+  obj.buildNub = function(col, row, frameIndex, scene) {
+    if (obj.nub) obj.nub.destroy();
+    if (obj.leaf) obj.leaf.destroy();
+
+    console.log("nub"+col+","+row);
+
+    obj.nub = makeTreeSprite(col, row, frameIndex, scene);
+    obj.leaf = makeTreeSprite(col, row, treeSprite.leaf, scene);
+
+    return 1;
+  }
+
+  return obj
+}
+
+// Expands nubs and replies from a given node. node should already be built
+// Returns how many columns wide the total growth was, minimum is 1
+function growTreeNode(node, rightOffset, depth, scene) {
+  if (node.replies.length < 1) {
+    return node.buildNub(rightOffset, depth + 1, treeSprite.downNub, scene);
+  } else {
+
+    //grow first reply
+    var reply = node.replies[0];
+    var branchWidth = reply.buildNodeSprites(rightOffset, depth + 1, 1, treeSprite.downIntersection, scene);
+    var extraWidth = growTreeNode(reply, rightOffset, depth + 1, scene);
+
+    //loop through replies after the first
+    for(var repliesIndex = 1; repliesIndex < node.replies.length; repliesIndex++) {
+      reply = node.replies[repliesIndex];
+
+      branchWidth+= reply.buildNodeSprites(rightOffset + branchWidth, depth + 1, extraWidth, treeSprite.rightIntersection, scene);
+
+      extraWidth = growTreeNode(reply, rightOffset + branchWidth, depth + 1, scene);
+    }
+
+    branchWidth += node.buildNub(rightOffset + branchWidth, depth + 1, treeSprite.rightNub, scene);
+
+    return branchWidth;
   }
 }
 
@@ -58,7 +145,8 @@ window.onload = function() {
   var gameConfig = {
     width: gameOptions.tileSize * gameOptions.tilesWide,
     height: gameOptions.tileSize * gameOptions.tilesHigh,
-    scene: [bootGame, playGame]
+    scene: [bootGame, playGame]//,
+    //backgroundColor: 0x00ffff
   }
   game = new Phaser.Game(gameConfig);
   window.focus()
